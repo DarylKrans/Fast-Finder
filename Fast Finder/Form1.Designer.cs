414d79b19a1e00a89de4df73a5774bd285635f24
Daryl Krans <wadzinsky@hotmail.com>
2023-03-05 19:10:47 -0600

    Quickly locate files on your computer.  A multithreaded application for massive file location operations in a short time period

---
+++ Fast Finder/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Fast_Finder
+{
+    partial class Fast_Finder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Fast_Finder));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listb1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox1.Location = new System.Drawing.Point(531, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(257, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(713, 314);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Rescan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // listb1
+            // 
+            this.listb1.FormattingEnabled = true;
+            this.listb1.Location = new System.Drawing.Point(6, 17);
+            this.listb1.Name = "listb1";
+            this.listb1.Size = new System.Drawing.Size(519, 329);
+            this.listb1.TabIndex = 2;
+            this.listb1.DoubleClick += new System.EventHandler(this.ListBox1_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(531, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(531, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Enter files to search for";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(531, 61);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(164, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(531, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Look for matches in";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 1);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "label4";
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(713, 59);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 11;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // Fast_Finder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 349);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listb1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Fast_Finder";
+            this.Text = "Fast Finder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listb1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button start;
+    }
+}
+
---
+++ Fast Finder/Form1.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fast_Finder
+{
+    public partial class Fast_Finder : Form
+    {
+        public readonly string cpath = $@"{(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData))}\FastFinder";
+        public readonly string cfile = @"\filelist.000";
+        public readonly string[] CBData = new string[4] { "Files and Folder Names", "Only File Names", "Only Folder Names", "File Extension" };
+        public string search = "";
+
+        public static class Files
+        {
+            public static System.Windows.Forms.Label[] label = new System.Windows.Forms.Label[0];
+            public static string[] Drives;
+            public static string[] FileList;
+            public static int[] Fnum = new int[0];
+            public static ConcurrentBag<string> folder_names = new ConcurrentBag<string>();
+            public static ConcurrentBag<string> file_names = new ConcurrentBag<string>();
+            public static int Folnum = 0;
+        }
+        public bool Labels = false;
+        public bool working = false;
+        public bool cancel = false;
+        public bool loaded = false;
+        public Fast_Finder()
+        {
+            InitializeComponent();
+            listb1.Sorted = true;
+            listb1.HorizontalScrollbar = true;
+            label4.Text = string.Empty;
+            comboBox1.DataSource = CBData; comboBox1.SelectedIndex = 0;
+            label1.Text = string.Empty;
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+        }
+        public static byte[] Compress(byte[] data)
+        {
+            MemoryStream output = new MemoryStream();
+            using (DeflateStream dstream = new DeflateStream(output, CompressionLevel.Optimal))
+            {
+                dstream.Write(data, 0, data.Length);
+            }
+            return output.ToArray();
+        }
+        public static string[] Decompress(byte[] data)
+        {
+            MemoryStream input = new MemoryStream(data);
+            MemoryStream output = new MemoryStream();
+            using (DeflateStream dstream = new DeflateStream(input, CompressionMode.Decompress))
+            {
+                dstream.CopyTo(output);
+            }
+            var decompdata = output.ToArray();
+            input.Dispose(); output.Dispose();
+            input.Close(); output.Close();
+
+            List<string> list = new List<string>();
+            int i = 0;
+            var buffer = new MemoryStream();
+            var write = new BinaryWriter(buffer);
+            while (i < decompdata.Length)
+            {
+                if (decompdata[i] != '\n') { write.Write(decompdata[i]); i++; }
+                else
+                {
+                    byte[] temp = buffer.ToArray();
+                    list.Add(Encoding.UTF8.GetString(temp));
+                    buffer.SetLength(0); i++;
+                }
+            }
+            buffer.Dispose(); write.Dispose();
+            buffer.Close(); write.Close();
+            return list.ToArray();
+        }
+        async void Start_Search()
+        {
+            if (!working)
+            {
+                working = true;
+                listb1.Items.Clear();
+                int t = 0; int m = 0;
+                Thread u = new Thread(new ThreadStart(Upd));
+                u.Start();
+                start.Text = "Cancel";
+                await Task.Run(() =>
+                {
+                    var cond = 0;
+                    var comp = "";
+                    Invoke(new Action(() => cond = comboBox1.SelectedIndex));
+                    for (int i = 0; i < Files.FileList.Length; i++)
+                    {
+                        if (cancel) break;
+                        switch(cond)
+                        {
+                            case 0: comp = Files.FileList[i]; break;
+                            case 1: comp = Path.GetFileNameWithoutExtension(Files.FileList[i]); break;
+                            case 2: comp = Path.GetDirectoryName(Files.FileList[i]); break;
+                            case 3: comp = Path.GetExtension(Files.FileList[i]); break;
+                        }
+                        t = i;
+                        if (comp.Contains(search))
+                        {
+                            if (!listb1.Items.Contains(Files.FileList[i]))
+                            {
+                                if (File.Exists(Files.FileList[i])) Invoke(new Action(() => listb1.Items.Add(Files.FileList[i])));
+                            }
+                            m++;
+                        }
+                    }
+                    working = false;
+                    Thread.Sleep(150);
+                    cancel = false;
+                    Invoke(new Action(() => start.Text = "Start"));
+                });
+
+                void Upd()
+                {
+                    while (t != Files.FileList.Length - 1)
+                    {
+                        Thread.Sleep(100);
+                        if (cancel) break;
+                        Invoke(new Action(() => { label4.Text = $"Searched {t + 1:N0} / Matches ({m + 1:N0})"; }));
+                    }
+                }
+            }
+
+        }
+        async void Load_List()
+        {
+            if (!Directory.Exists(cpath)) Directory.CreateDirectory(cpath);
+            if (!File.Exists(cpath + cfile)) Populate_Files();
+            else
+            {
+                label1.Text = "Loading Saved File List";
+                await Task.Run(() =>
+                {
+                    Get_Drives();
+                    string[] dl = new string[Files.Drives.Length];
+                    int[] df = new int[Files.Drives.Length];
+                    for (int i = 0; i < dl.Length; i++) dl[i] = Files.Drives[i].Split(':').First();
+                    Files.FileList = Decompress(File.ReadAllBytes(cpath + cfile));
+                    if (!Labels) Invoke(new Action(() => Set_Labels(Files.Drives.Length + 1)));
+                    for (int p = 0; p < Files.FileList.Length; p++)
+                    {
+                        var c = Files.FileList[p].Split(':').First();
+                        for (int i = 0; i < dl.Length; i++)
+                        {
+                            if (c == dl[i]) df[i] += 1;
+                        }
+                    }
+                    Invoke(new Action(() =>
+                    {
+                        for (int i = 0; i < dl.Length; i++)
+                        {
+                            Files.label[i].Text = $"{Files.Drives[i]} {df[i]:N0}";
+                        }
+                        Files.label[Files.label.Length - 1].Text = $"Total ({df.Sum():N0})";
+                        label1.Text = "";
+                    }));
+                });
+            }
+        }
+        void Set_Labels(int t)
+        {
+            Files.label = new System.Windows.Forms.Label[t];
+            for (int i = 0; i < Files.label.Length; i++)
+            {
+                Files.label[i] = new System.Windows.Forms.Label()
+                {
+                    AutoSize = true,
+                    Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0),
+                    Location = new System.Drawing.Point(540, 100 + (i * 13)),
+                };
+                Controls.Add(Files.label[i]);
+            }
+            Labels = true;
+        }
+        void Get_Drives()
+        {
+            var d = System.IO.DriveInfo.GetDrives();
+            List<string> list = new List<string>();
+            list.Sort();
+            for (int i = 0; i < d.Length; i++)
+            {
+                list.Add(d[i].ToString());
+            }
+            Files.Drives = list.ToArray();
+            list.Clear();
+        }
+        private async void Populate_Files()
+        {
+            if (!working)
+            {
+                working = true;
+                button1.Enabled = false; button1.Text = "Scanning";
+                Get_Drives();
+                Thread[] EF = new Thread[Files.Drives.Length];
+                Files.Fnum = new int[Files.Drives.Length];
+                if (!Labels) Set_Labels(Files.Drives.Length + 1);
+                bool[] parse = new bool[Files.Drives.Length];
+                await Task.Run(() =>
+                {
+                    Invoke(new Action(() => label1.Text = "Getting File List."));
+                    Thread c = new Thread(() => Upd());
+                    c.Start();
+                    for (int i = 0; i < Files.Drives.Length; i++)
+                    {
+                        parse[i] = false;
+                        var u = i;
+                        EF[i] = new Thread(() => Enum_List(Files.Drives[u], u));
+                        EF[i].Start();
+                    }
+                    for (int i = 0; i < Files.Drives.Length; i++) EF[i].Join();
+                    c.Abort();
+                    Invoke(new Action(() => label1.Text = "Sorting File List"));
+                    CompList();
+                    Files.FileList = Decompress(File.ReadAllBytes(cpath + cfile));
+                    Invoke((Action)(() => { label1.Text = ""; button1.Enabled = true; button1.Text = "Rescan"; }));
+                    working = false;
+
+                });
+
+                void CompList()
+                {
+                    List<string> list = Files.file_names.ToList();
+                    list.Sort();
+                    Files.file_names = new ConcurrentBag<string>();
+                    var buffer = new MemoryStream();
+                    var write = new BinaryWriter(buffer);
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        byte[] p = Encoding.ASCII.GetBytes(list[i]);
+                        write.Write(p);
+                    }
+                    byte[] bytes = buffer.ToArray();
+                    buffer.Flush();
+                    buffer.Close();
+                    list.Clear();
+                    var comp = Compress(bytes);
+                    File.WriteAllBytes(cpath + cfile, comp);
+                    Array.Clear(bytes, 0, bytes.Length);
+                }
+
+                void Enum_List(string drive, int i)
+                {
+                    string[] fi;
+                    Files.Fnum[i] = 0;
+                    fi = Get(drive).ToArray();
+                    if (fi.Length > 0) { parse[i] = true; Process_List(fi); }
+
+                    IEnumerable<string> Get(string path)
+                    {
+                        IEnumerable<string> f = Enumerable.Empty<string>();
+                        IEnumerable<string> d = Enumerable.Empty<string>();
+                        try
+                        {
+                            var permission = new FileIOPermission(FileIOPermissionAccess.Read, path);
+                            permission.Demand();
+                            f = Directory.GetFiles(path);
+                            d = Directory.GetDirectories(path);
+                        }
+                        catch { path = null; }
+                        foreach (var file in f)
+                        {
+                            Files.Fnum[i]++;
+                            yield return file;
+                        }
+                        var subdirectoryItems = d.SelectMany(Get);
+                        foreach (var result in subdirectoryItems)
+                        {
+                            yield return result; // result;
+                        }
+                    }
+                }
+                void Process_List(string[] files)
+                {
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        Files.file_names.Add(files[i] + '\n');
+                    }
+                }
+                void Upd()
+                {
+                    Thread.Sleep(200);
+                    while (1 == 1)
+                    {
+                        Invoke(new Action(() =>
+                        {
+                            for (int i = 0; i < Files.label.Length - 1; i++)
+                            {
+                                if (EF[i].IsAlive)
+                                {
+                                    if (!parse[i]) Files.label[i].Text = $"{Files.Drives[i]} {Files.Fnum[i]:N0}";
+                                }
+                                if (!EF[i].IsAlive)
+                                {
+                                    if (Files.Fnum[i] > 0) Files.label[i].Text = $"{Files.Drives[i]} {Files.Fnum[i]:N0}";
+                                    else Files.label[i].Text = $"{Files.Drives[i]} No Files!";
+                                }
+                            }
+                            Files.label[Files.Drives.Length].Text = $"(Total {Files.Fnum.Sum():N0})";
+                        }));
+                        Thread.Sleep(50);
+                    }
+                }
+            }
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Populate_Files();
+        }
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            loaded = true;
+            Load_List();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0) search = textBox1.Text.ToString();
+        }
+        private void ListBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listb1.SelectedItem != null)
+            {
+                if (listb1.SelectedItem.ToString().Length != 0)
+                {
+                    string filePath = listb1.SelectedItem.ToString();
+                    if (!File.Exists(listb1.SelectedItem.ToString()))
+                    {
+                        return;
+                    }
+                    string argument = "/select, \"" + filePath + "\"";
+                    System.Diagnostics.Process.Start("explorer.exe", argument);
+                }
+            }
+        }
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == (char)Keys.Enter)
+            {
+                e.Handled = e.SuppressKeyPress = true;
+                if (textBox1.Text.Length > 0)
+                {
+                    if (search.Length > 0) Start_Search();
+                }
+            }
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            if (working) cancel = true;
+            else Start_Search();
+        }
+
+        private async void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (loaded)
+            {
+                if (!working && search.Length > 0) Start_Search();
+                else
+                {
+                    cancel = true;
+                    while (cancel == true)
+                    { await Task.Delay(50); }
+                    if (search.Length > 0) Start_Search();
+                }
+            }
+        }
+    }
+}
